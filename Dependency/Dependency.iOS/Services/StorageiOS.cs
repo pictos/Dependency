130caa531e124d7aa9a9b08e4be80183c35f9870
Pedro Jesus <pedro.pj.souza@hotmail.com>
2018-10-04 15:50:48 -0300

    Changes: _ iOS Implementation

---
+++ Dependency/Dependency.iOS/Services/StorageiOS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dependency.iOS.Services;
+using Dependency.Services;
+using Foundation;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(StorageiOS))]
+
+namespace Dependency.iOS.Services
+{
+    public class StorageiOS : IStorage
+    {
+        public Task<UInt64> GetFreeSpace()
+        {
+           // var personalFolder = NSFileManager.DefaultManager.GetFileSystemAttributes(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+            var applicationFolder = NSFileManager.DefaultManager.GetFileSystemAttributes(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+
+            return Task.FromResult(applicationFolder.FreeSize);
+        }
+    }
+}
