d693185e0396aedce54d40e121074891bf6c174c
Pedro Jesus <pedro.pj.souza@hotmail.com>
2018-11-02 14:31:56 -0300

    sizeiOS

--- Dependency/Dependency.iOS/Services/StorageiOS.cs
+++ Dependency/Dependency.iOS/Services/StorageiOS.cs
@@ -13,9 +13,10 @@ namespace Dependency.iOS.Services
     {
         public Task<UInt64> GetFreeSpace()
         {
-           // var personalFolder = NSFileManager.DefaultManager.GetFileSystemAttributes(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
-            var applicationFolder = NSFileManager.DefaultManager.GetFileSystemAttributes(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
-
+            // var personalFolder = NSFileManager.DefaultManager.GetFileSystemAttributes(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+            NSFileSystemAttributes applicationFolder = NSFileManager.DefaultManager.GetFileSystemAttributes(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            var freeSpace = applicationFolder.FreeSize;
+            var totalSpace = applicationFolder.Size;
             return Task.FromResult(applicationFolder.FreeSize);
         }
     }
