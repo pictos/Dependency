bc7714a5270b181b69916b74625fe0dfdd99d60a
Pedro Jesus <pedro.pj.souza@hotmail.com>
2018-11-02 14:55:30 -0300

    AndroidStorage

--- Dependency/Dependency.Android/Services/StorageAndroid.cs
+++ Dependency/Dependency.Android/Services/StorageAndroid.cs
@@ -13,6 +13,15 @@ namespace Dependency.Droid.Services
     {
         public Task<UInt64> GetFreeSpace()
         {
+            var fullExternalStorage = Android.OS.Environment.ExternalStorageDirectory.TotalSpace;
+            var freeExternalStorage = Android.OS.Environment.ExternalStorageDirectory.UsableSpace;
+
+            var fullInternalStorage = Android.OS.Environment.RootDirectory.TotalSpace;
+            var freeInternalStorage = Android.OS.Environment.RootDirectory.UsableSpace;
+
+
+
+            //Using StatFS
             var path             = new StatFs(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData));
             long blockSize       = path.BlockSizeLong;
             long avaliableBlocks = path.AvailableBlocksLong;
